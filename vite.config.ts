db5e33186fd422ce17fb5835081759989b54b8fa
Sion Lee <35111239+sionara@users.noreply.github.com>
2024-04-04 17:38:21 -0400

    added homepage in package.json

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: { port: 3000 },
   base: "/capstone-GameUI",
+  server: { port: 3000 },
 });
