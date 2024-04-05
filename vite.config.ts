8563dbc0ae74129eb2922d8f3166db7b0403f028
Sion Lee <35111239+sionara@users.noreply.github.com>
2024-04-04 21:28:37 -0400

    changed base in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/capstone-GameUI",
+  base: "/",
   server: { port: 3000 },
 });
